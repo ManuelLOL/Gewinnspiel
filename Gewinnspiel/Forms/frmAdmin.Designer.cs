49c613c0fabcfd7dea5f9c1aae2a59ac5d602345
Manuel Boehm <Manuel.Boehm@s.hakmistelbach.ac.at>
2021-03-23 08:12:57 +0100

    frmTeilnehmer

--- Gewinnspiel/Forms/frmAdmin.Designer.cs
+++ Gewinnspiel/Forms/frmAdmin.Designer.cs
@@ -28,12 +28,141 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gewinnspielToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hinzufügenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bearbeitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teilnehmerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teilnehmerlisteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hinzufügenToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.bearbeitenToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.deAktivierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.einemGewinnspielHinzufügenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gewinnspielToolStripMenuItem,
+            this.teilnehmerToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(779, 29);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gewinnspielToolStripMenuItem
+            // 
+            this.gewinnspielToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hinzufügenToolStripMenuItem,
+            this.bearbeitenToolStripMenuItem});
+            this.gewinnspielToolStripMenuItem.Name = "gewinnspielToolStripMenuItem";
+            this.gewinnspielToolStripMenuItem.Size = new System.Drawing.Size(110, 25);
+            this.gewinnspielToolStripMenuItem.Text = "Gewinnspiel";
+            // 
+            // hinzufügenToolStripMenuItem
+            // 
+            this.hinzufügenToolStripMenuItem.Name = "hinzufügenToolStripMenuItem";
+            this.hinzufügenToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.hinzufügenToolStripMenuItem.Text = "Hinzufügen";
+            // 
+            // bearbeitenToolStripMenuItem
+            // 
+            this.bearbeitenToolStripMenuItem.Name = "bearbeitenToolStripMenuItem";
+            this.bearbeitenToolStripMenuItem.Size = new System.Drawing.Size(180, 26);
+            this.bearbeitenToolStripMenuItem.Text = "Bearbeiten";
+            // 
+            // teilnehmerToolStripMenuItem
+            // 
+            this.teilnehmerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teilnehmerlisteToolStripMenuItem,
+            this.hinzufügenToolStripMenuItem1,
+            this.bearbeitenToolStripMenuItem1,
+            this.deAktivierenToolStripMenuItem,
+            this.einemGewinnspielHinzufügenToolStripMenuItem});
+            this.teilnehmerToolStripMenuItem.Name = "teilnehmerToolStripMenuItem";
+            this.teilnehmerToolStripMenuItem.Size = new System.Drawing.Size(102, 25);
+            this.teilnehmerToolStripMenuItem.Text = "Teilnehmer";
+            // 
+            // teilnehmerlisteToolStripMenuItem
+            // 
+            this.teilnehmerlisteToolStripMenuItem.Name = "teilnehmerlisteToolStripMenuItem";
+            this.teilnehmerlisteToolStripMenuItem.Size = new System.Drawing.Size(302, 26);
+            this.teilnehmerlisteToolStripMenuItem.Text = "Teilnehmerliste";
+            // 
+            // hinzufügenToolStripMenuItem1
+            // 
+            this.hinzufügenToolStripMenuItem1.Name = "hinzufügenToolStripMenuItem1";
+            this.hinzufügenToolStripMenuItem1.Size = new System.Drawing.Size(302, 26);
+            this.hinzufügenToolStripMenuItem1.Text = "Hinzufügen";
+            // 
+            // bearbeitenToolStripMenuItem1
+            // 
+            this.bearbeitenToolStripMenuItem1.Name = "bearbeitenToolStripMenuItem1";
+            this.bearbeitenToolStripMenuItem1.Size = new System.Drawing.Size(302, 26);
+            this.bearbeitenToolStripMenuItem1.Text = "Bearbeiten";
+            // 
+            // deAktivierenToolStripMenuItem
+            // 
+            this.deAktivierenToolStripMenuItem.Name = "deAktivierenToolStripMenuItem";
+            this.deAktivierenToolStripMenuItem.Size = new System.Drawing.Size(302, 26);
+            this.deAktivierenToolStripMenuItem.Text = "De/Aktivieren";
+            // 
+            // einemGewinnspielHinzufügenToolStripMenuItem
+            // 
+            this.einemGewinnspielHinzufügenToolStripMenuItem.Name = "einemGewinnspielHinzufügenToolStripMenuItem";
+            this.einemGewinnspielHinzufügenToolStripMenuItem.Size = new System.Drawing.Size(302, 26);
+            this.einemGewinnspielHinzufügenToolStripMenuItem.Text = "einem Gewinnspiel hinzufügen";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(82, 25);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(75, 25);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // frmAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(779, 438);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmAdmin";
             this.Text = "frmAdmin";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gewinnspielToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hinzufügenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bearbeitenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerlisteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hinzufügenToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem bearbeitenToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem deAktivierenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem einemGewinnspielHinzufügenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
     }
 }
---
+++ Gewinnspiel/Forms/frmTeilnehmerListe.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Gewinnspiel.Forms
+{
+    partial class frmTeilnehmerListe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.teilnehmerHinzufügenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teilnehmerBearbeitenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teilnehmerDeaktivierenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader9 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8,
+            this.columnHeader9});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(25, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(763, 376);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.teilnehmerHinzufügenToolStripMenuItem,
+            this.teilnehmerBearbeitenToolStripMenuItem,
+            this.teilnehmerDeaktivierenToolStripMenuItem,
+            this.teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(396, 108);
+            // 
+            // teilnehmerHinzufügenToolStripMenuItem
+            // 
+            this.teilnehmerHinzufügenToolStripMenuItem.Name = "teilnehmerHinzufügenToolStripMenuItem";
+            this.teilnehmerHinzufügenToolStripMenuItem.Size = new System.Drawing.Size(395, 26);
+            this.teilnehmerHinzufügenToolStripMenuItem.Text = "Teilnehmer hinzufügen";
+            // 
+            // teilnehmerBearbeitenToolStripMenuItem
+            // 
+            this.teilnehmerBearbeitenToolStripMenuItem.Name = "teilnehmerBearbeitenToolStripMenuItem";
+            this.teilnehmerBearbeitenToolStripMenuItem.Size = new System.Drawing.Size(395, 26);
+            this.teilnehmerBearbeitenToolStripMenuItem.Text = "Teilnehmer bearbeiten ";
+            // 
+            // teilnehmerDeaktivierenToolStripMenuItem
+            // 
+            this.teilnehmerDeaktivierenToolStripMenuItem.Name = "teilnehmerDeaktivierenToolStripMenuItem";
+            this.teilnehmerDeaktivierenToolStripMenuItem.Size = new System.Drawing.Size(395, 26);
+            this.teilnehmerDeaktivierenToolStripMenuItem.Text = "Teilnehmer de/aktivieren";
+            // 
+            // teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem
+            // 
+            this.teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem.Name = "teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem";
+            this.teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem.Size = new System.Drawing.Size(395, 26);
+            this.teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem.Text = "Teilnehmer zum GewinnSpiel hinzufügen";
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "TID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Zuname";
+            this.columnHeader2.Width = 87;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Vorname";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Email";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Geschlecht";
+            this.columnHeader5.Width = 104;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Geburtsdatum";
+            this.columnHeader6.Width = 143;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Admin";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Deaktiviert";
+            this.columnHeader8.Width = 94;
+            // 
+            // columnHeader9
+            // 
+            this.columnHeader9.Text = "Spiele";
+            // 
+            // frmTeilnehmerListe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "frmTeilnehmerListe";
+            this.Text = "frmTeilnehmerListe";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+        private System.Windows.Forms.ColumnHeader columnHeader9;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerHinzufügenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerBearbeitenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerDeaktivierenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem teilnehmerZumGewinnSpielHinzufügenToolStripMenuItem;
+    }
+}
